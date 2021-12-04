2cb916edda044bca97f7cb4b3b36966a3cb74557
Quzeem <quzeemagbaje@gmail.com>
2021-12-04 23:58:52 +0100

    Implement signup feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeOrmConfig from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig()),
     TasksModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
---
+++ src/auth/dto/auth.credentials.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/,
+    {
+      message:
+        'Password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character',
+    },
+  )
+  password: string;
+}
